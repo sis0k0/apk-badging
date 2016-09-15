61bfadb61fd6fb11ca3d1f656633e2c79734ec39
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2016-09-15 11:36:04 +0300

    Fix install file to work for windows

--- install.js
+++ install.js
@@ -6,7 +6,13 @@ const os = require('os');
 const path = require('path');
 const exec = require('child_process').exec;
 
+const AdmZip = require('adm-zip');
+const ProgressBar = require('progress');
+
 const toolsDirectory = require('./config/tools').directory;
+
+let DO_CHMOD = true;
+let aaptExecutable = 'aapt';
 
 const SUPPORTED_PLATFORMS = {
     Linux: 'linux',
@@ -15,10 +21,12 @@ const SUPPORTED_PLATFORMS = {
 };
 
 (function downloadAapt() {
-    let platform = getPlatformType();
+    let platform = getPlatform()[0];
+    let arch = getPlatform()[1];
+    let url = getUrl(platform, arch);
+
     let targetDir = getTargetDir();
 
-    let url = getUrl(platform);
 
     fetchFile(url).then(unzip);
 })();
@@ -27,18 +35,32 @@ process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
-function getUrl(platform) {
-    return `http://dl-ssl.google.com/android/repository/platform-tools_r16-${platform}.zip`;
+function getUrl(platform, arch) {
+    if (platform === 'macosx') {
+        return 'http://dl.google.com/android/adt/22.6.2/adt-bundle-mac-x86_64-20140321.zip'; 
+    }
+    
+    if (arch === 'x64') {
+        return `http://dl.google.com/android/adt/22.6.2/adt-bundle-${platform}-x86-20140321.zip`;
+    } else {
+        return `http://dl.google.com/android/adt/22.6.2/adt-bundle-${platform}-x86_64-20140321.zip`;
+    }
 }
 
-function getPlatformType() {
+function getPlatform() {
     let platform = SUPPORTED_PLATFORMS[os.type()];
+    let arch = os.arch();
 
     if (typeof platform === 'undefined') {
         throw new Error('Unknown OS!');
     }
+    
+    if (platform === 'windows') {
+        DO_CHMOD = false;
+        aaptExecutable += '.exe';
+    }
 
-    return platform;
+    return [platform, arch];
 }
 
 function getTargetDir() {
@@ -58,8 +80,17 @@ function fetchFile(url) {
     let data = fs.createWriteStream(file);
 
     return new Promise(function(resolve, reject) {
-        http.get(url, function (response) {
+        let request = http.get(url, function (response) {
+            let len = parseInt(response.headers['content-length'], 10);
+            let bar = new ProgressBar('Downloading Android ADT Bundle [:bar] :percent :current of :total', {
+                complete: '=',
+                incomplete: ' ',
+                width: 20,
+                total: len
+            });
+
             response.pipe(data);
+            response.on('data', chunk => bar.tick(chunk.length));
             response.on('error', error => reject(error));
             response.on('end', () => resolve(file));
         });
@@ -67,22 +98,45 @@ function fetchFile(url) {
 }
 
 function getTemporaryFileLocation() {
-    return `/tmp/platform-tools-${new Date().getTime()}.zip`;
+    let tmpDir = path.join(__dirname, 'tmp');
+    try {
+        fs.statSync(tmpDir);
+    } catch (e) {
+        fs.mkdirSync(tmpDir);
+    } 
+    
+    return `./tmp/platform-tools-${new Date().getTime()}.zip`;
 }
 
 function unzip(file) {
-    return new Promise(function(resolve, reject) {
-        let command = `unzip -j -o ${file} platform-tools/aapt -d ${toolsDirectory}`;
+    return new Promise(function(resolve, reject) { 
+        let zip = new AdmZip(file);
 
-        exec(command, error => {
-            if (error) {
-                reject(error);
+        try {
+            let zipEntries = zip.getEntries();
+            let entryName = false;
+            
+            zipEntries.forEach(function(zipEntry) {
+                if (zipEntry.entryName.match(/aapt/)) {
+                entryName = zipEntry.entryName;
+                }
+            });
+            if (!entryName) {
+                reject(new Error('Couldn\'t locate aapt application.'));
             }
-
-            let filePath = path.join(toolsDirectory, 'aapt');
-            fs.chmodSync(filePath, '755');
+            
+            zip.extractEntryTo(entryName, toolsDirectory, false, true);
+            
+            if (DO_CHMOD) {
+                let filePath = path.join(toolsDirectory, aaptExecutable);
+                fs.chmodSync(filePath, '755');
+            }
+            
             fs.unlinkSync(file);
             resolve();
-        });
+        } catch(error) {
+            fs.unlinkSync(file);
+            reject(error);
+        }
     });
 }
